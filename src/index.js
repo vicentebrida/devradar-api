a0baac999433a3a9de06d305f34678d420df257d
Vicente Brida <vicentebrida@outlook.com>
2020-01-15 08:49:26 -0300

    feat: Add cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 });
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 
